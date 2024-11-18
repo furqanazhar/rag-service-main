2efb314efe0bde561a029d776c87b5a7d8f649bf
Furqan Azhar - AAI <furqanazhar92@gmail.com>
2024-11-18 16:48:43 +0500

    added get patents endpoint

--- fastapi_service/main.py
+++ fastapi_service/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from elasticsearch import Elasticsearch
 
 app = FastAPI()
+es = Elasticsearch("http://elasticsearch:9200")
+
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to FastAPI!"}
+
+
+@app.get("/patents/{company}")
+async def get_patents(company: str):
+    response = es.search(index="family_g1_v2",
+                         query={"term":
+                                    {"members.best_standardized_name.name.keyword": company}
+                                }
+                         )
+    result = []
+    for hit in response["hits"]["hits"]:
+        result.append(
+            {"family": hit["_source"]["family_id"],
+             "members": hit["_source"]["members"],
+             "embedding": hit["_source"]["embeddings_768_bgebase"]
+             }
+        )
+    return result
